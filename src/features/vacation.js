1d9c988d4c8e2955164b7a03713e3505cdae68fc
Hong Jiang <j@1byte.io>
2019-06-06 12:34:01 +0800

    fix: minor correction to lunar date display

--- src/features/vacation.js
+++ src/features/vacation.js
@@ -3,7 +3,6 @@ const CalendarChinese = require('date-chinese');
 function handleVacation(lc, ctx) {
   const dateStr = ctx.matches[1];
   const respond = ctx.respond;
-  console.log(`Date: ${dateStr}`);
   var query = new lc.Query('Leave');
   var startOfToday =
     dateStr && dateStr.length > 0 ? new Date(dateStr) : new Date();
@@ -18,27 +17,23 @@ function handleVacation(lc, ctx) {
   query.greaterThanOrEqualTo('endDate', startOfToday);
   query.find().then(
     results => {
-      console.log('found');
       results = results.filter(result => {
         const endDate = result.get('endDate');
         const endTime = result.get('endTime');
         return endDate > endOfToday || endTime == 'PM';
       });
-      console.log('calculating date');
       const year = startOfToday.getFullYear();
       const month = startOfToday.getMonth() + 1;
       const day = startOfToday.getDate();
-      console.log('calculating chinese date');
       const cal = new CalendarChinese.CalendarChinese();
       cal.fromDate(startOfToday);
       let zzDay = cal.day;
-      if (zzDay < 10) {
+      if (zzDay < 11) {
         zzDay = `初 ${zzDay}`;
       } else {
         zzDay = ` ${zzDay}`;
       }
       const zzYear = year - 1926;
-      console.log('calculated');
       if (results.length == 0) {
         respond(
           `${year} 年 ${month} 月 ${day} 日（长者 ${zzYear} 年 ${
